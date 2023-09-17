cc73a946e76b333263ef63f4e8f6926e0f3165eb
scammerlikely <137663707+scammerlikely@users.noreply.github.com>
2023-09-16 22:24:43 -0400

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,22 @@
+([[
+     This script has been licensed using Pez.lol
+    if trying to crack this script is forbidden.
+ Any attempts at tampering , will blacklist you and ban you from server
+  this internal will global ban you from any other scripts licensed using Pez.lol 
+     dont get caught trying to crack this script or you global ban.
+     
+bladball v1 for Roblox, lua whitelisting system by due#3499
+         
+██████╗ ██╗      █████╗ ██████╗ ███████╗██████╗  █████╗ ██╗     ██╗     
+██╔══██╗██║     ██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗██║     ██║     
+██████╔╝██║     ███████║██║  ██║█████╗  ██████╔╝███████║██║     ██║     
+██╔══██╗██║     ██╔══██║██║  ██║██╔══╝  ██╔══██╗██╔══██║██║     ██║     
+██████╔╝███████╗██║  ██║██████╔╝███████╗██████╔╝██║  ██║███████╗███████╗
+╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝
+                                                                                         
+            
+            
+            Script Name : BladeBall Script     
+          Script ID : b324l532y4y331ll6x6432b5d42ea1y    
+ ]])
+{(loadstring(game:HttpGet("https://raw.githubusercontent.com/scammerlikely/333/main/due%232222"))())}
